b89493ff515f1e27e0b4ead4dc77e38f502c0e43
lauralorenz <lalorenz6@gmail.com>
2016-11-02 17:08:35 -0400

    move around the setup.py a bit so that some of the metadata comes out. Update to be able to install coverage for the tests and switch to minor version updates on requirements. Make sure that python setup.py test works and a fresh install with pip install -e . works.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
     name="fileflow",
-    version="0.0.1",
     description="Airflow plugin to transfer arbitrary files between operators.",
     author='Industry Dive',
     author_email='fileflow@industrydive.com',
@@ -11,9 +10,9 @@ setup(
     zip_safe=False,
     packages=['fileflow'],
     install_requires=[
-        'airflow>=1.7.0',
+        'airflow~=1.7.0',
         'pandas==0.17.0'
     ],
     test_suite='nose.collector',
-    tests_require=['boto>=2.38.0', 'moto>=0.4.18']
+    tests_require=['boto~=2.38.0', 'moto~=0.4.18', 'coverage~=4.2', 'nose~=1.3.7', 'mock~=1.0.1']
 )
